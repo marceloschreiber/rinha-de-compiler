52eb747d2990efff5dff52c1bc98ba5d29fcd4d0
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2023-09-23 03:28:09 -0300

    Tail call

--- src/core.clj
+++ src/core.clj
@@ -33,37 +33,39 @@
 
 (defn eval
   [expression env]
-  (case (:kind expression)
-    "Print"  (let [value (eval (:value expression) env)]
-               (println (cond
-                          (coll? value) (format "(%s, %s)" (first value) (second value))
-                          (map? value) "<#closure>"
-                          :else value))
-               value)
-    "Binary" (let [[lhs rhs] (get-params expression env)]
-               (case (:op expression)
-                 "And" (and lhs rhs)
-                 "Or" (or lhs rhs)
-                 ((get env (:op expression)) lhs rhs)))
-    "Bool" (:value expression)
-    "Int" (:value expression)
-    "Str" (:value expression)
-    "Tuple" (get-params expression env)
-    "If" (eval (if (eval (:condition expression) env)
-                 (:then expression)
-                 (:otherwise expression))
-               env)
-    "First" (first (get-params (:value expression) env))
-    "Second" (second (get-params (:value expression) env))
-    "Let" (eval (:next expression)
-                (assoc env (-> expression :name :text) (eval (:value expression) env)))
-    "Var" (get env (:text expression))
-    "Function" {:parameters (map :text (:parameters expression))
-                :body       (:value expression)}
-    "Call" (let [callee    (eval (:callee expression) env)
-                 arguments (map #(eval % env) (:arguments expression))]
-             (eval (:body callee)
-                   (merge env (zipmap (:parameters callee) arguments))))))
+  (loop [expression expression
+         env env]
+    (case (:kind expression)
+      "Print"  (let [value (eval (:value expression) env)]
+                 (println (cond
+                            (coll? value) (format "(%s, %s)" (first value) (second value))
+                            (map? value) "<#closure>"
+                            :else value))
+                 value)
+      "Binary" (let [[lhs rhs] (get-params expression env)]
+                 (case (:op expression)
+                   "And" (and lhs rhs)
+                   "Or" (or lhs rhs)
+                   ((get env (:op expression)) lhs rhs)))
+      "Bool" (:value expression)
+      "Int" (:value expression)
+      "Str" (:value expression)
+      "Tuple" (get-params expression env)
+      "If" (recur (if (eval (:condition expression) env)
+                    (:then expression)
+                    (:otherwise expression))
+                  env)
+      "First" (first (get-params (:value expression) env))
+      "Second" (second (get-params (:value expression) env))
+      "Let" (recur (:next expression)
+                   (assoc env (-> expression :name :text) (eval (:value expression) env)))
+      "Var" (get env (:text expression))
+      "Function" {:parameters (map :text (:parameters expression))
+                  :body       (:value expression)}
+      "Call" (let [callee    (eval (:callee expression) env)
+                   arguments (map #(eval % env) (:arguments expression))]
+               (recur (:body callee)
+                      (merge env (zipmap (:parameters callee) arguments)))))))
 
-(let [expression (ast "resources/tuple.rinha")]
+(let [expression (ast "resources/sum.rinha")]
   (eval expression global-env))
